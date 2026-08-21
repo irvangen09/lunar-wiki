c2c8ae1ec7c4c0945a577ac9836cfd91aa4ac858
nymphadora07 <cocnymphadora@gmail.com>
2026-08-21 18:32:20 +0700

    feat: register content_type taxonomy

--- includes/Content/class-taxonomies.php
+++ includes/Content/class-taxonomies.php
@@ -15,12 +15,15 @@ class Taxonomies {
 
 	private const SLUG_GAME = 'game';
 
+	private const SLUG_CONTENT_TYPE = 'content_type';
+
 	public function init(): void {
 		add_action( 'init', array( $this, 'register' ) );
 	}
 
 	public function register(): void {
 		$this->register_game();
+		$this->register_content_type();
 	}
 
 	private function register_game(): void {
@@ -55,7 +58,43 @@ class Taxonomies {
 		);
 	}
 
+	private function register_content_type(): void {
+		$labels = array(
+			'name'          => __( 'Content Types', 'lunar-wiki' ),
+			'singular_name' => __( 'Content Type', 'lunar-wiki' ),
+			'search_items'  => __( 'Search Content Types', 'lunar-wiki' ),
+			'all_items'     => __( 'All Content Types', 'lunar-wiki' ),
+			'edit_item'     => __( 'Edit Content Type', 'lunar-wiki' ),
+			'update_item'   => __( 'Update Content Type', 'lunar-wiki' ),
+			'add_new_item'  => __( 'Add New Content Type', 'lunar-wiki' ),
+			'new_item_name' => __( 'New Content Type Name', 'lunar-wiki' ),
+			'menu_name'     => __( 'Content Type', 'lunar-wiki' ),
+		);
+
+		register_taxonomy(
+			self::SLUG_CONTENT_TYPE,
+			array( Post_Types::get_slug() ),
+			array(
+				'labels'            => $labels,
+				'hierarchical'      => false,
+				'public'            => true,
+				'show_ui'           => true,
+				'show_admin_column' => true,
+				'show_in_rest'      => true,
+				'query_var'         => true,
+				'rewrite'           => array(
+					'slug'       => 'content-type',
+					'with_front' => false,
+				),
+			)
+		);
+	}
+
 	public static function get_slug_game(): string {
 		return self::SLUG_GAME;
+	}
+
+	public static function get_slug_content_type(): string {
+		return self::SLUG_CONTENT_TYPE;
 	}
 }
