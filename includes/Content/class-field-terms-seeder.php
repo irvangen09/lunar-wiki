b6f96a315b1c279d4d0c6828d79884c60ef2d89d
nymphadora07 <cocnymphadora@gmail.com>
2026-08-22 08:41:58 +0700

    feat: seed initial "Role" term into wiki_field taxonomy

---
+++ includes/Content/class-field-terms-seeder.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Seeds the initial "Role" term into the wiki_field taxonomy.
+ *
+ * @package Lunar\Content
+ */
+
+namespace Lunar\Content;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Field_Terms_Seeder {
+
+	private const SEEDED_OPTION = 'lunar_wiki_field_terms_seeded';
+
+	private const INITIAL_TERM_NAME = 'Role';
+
+	public function init(): void {
+		add_action( 'admin_init', array( $this, 'maybe_seed' ) );
+	}
+
+	public function maybe_seed(): void {
+		if ( get_option( self::SEEDED_OPTION ) ) {
+			return;
+		}
+
+		if ( ! taxonomy_exists( Taxonomies::get_slug_field() ) ) {
+			return;
+		}
+
+		$existing = term_exists( self::INITIAL_TERM_NAME, Taxonomies::get_slug_field() );
+
+		if ( ! $existing ) {
+			$result = wp_insert_term( self::INITIAL_TERM_NAME, Taxonomies::get_slug_field() );
+
+			if ( is_wp_error( $result ) ) {
+				return;
+			}
+		}
+
+		update_option( self::SEEDED_OPTION, true );
+	}
+}
